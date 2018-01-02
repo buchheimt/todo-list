899b55aede5320fc75ccec26299b5cbc127644a2
Tyler Buchheim <tbuchhei@alumni.nd.edu>
2018-01-01 18:17:35 -0600

    Implement add Todo feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
-$("li").on("click", function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
-$("li span").on("click", function(e) {
+$("ul").on("click", "li span", function(e) {
     e.stopPropagation();
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
+});
+
+$("input[type='text']").on("keypress", function(e) {
+    if (e.which === 13) {
+        let todoText = $(this).val();
+        $(this).val("");
+        $("ul").append(`<li><span> X </span>${todoText}</li>`);
+    }
 });
